2e42c4a3d8037dd4f857a233de279f7def51416a
dwinovic <nd.cahya19@gmail.com>
2021-09-22 10:55:13 +0700

    initial commit and setup initial project - router, redux, styled component, and folder structure

---
+++ src/config/router/index.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
